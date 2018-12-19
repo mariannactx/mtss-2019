001382bde9ded86c046791254c7b972a8da1f29e
Marianna Cruz <mariannactx@gmail.com>
2018-12-19 12:20:10 -0300

    Ajustar posição dos botões do menu inscricao-o-evento

--- script.js
+++ script.js
@@ -2,15 +2,29 @@ window.addEventListener("load", function(){
     var pageWidth = window.innerWidth;
     var pageHeight = window.innerHeight - 66;
 
-    document.getElementById("inscricao").style.borderTopWidth = pageHeight + "px";
-    document.getElementById("inscricao").style.borderRightWidth = pageWidth + "px";
+    var inscricao = document.getElementById("inscricao");
+    var sobre = document.getElementById("o-evento");
     
-    document.getElementById("o-evento").style.borderBottomWidth = pageHeight + "px";
-    document.getElementById("o-evento").style.borderLeftWidth = pageWidth + "px";
+    inscricao.style.borderTopWidth = pageHeight + "px";
+    inscricao.style.borderRightWidth = pageWidth + "px";
+    sobre.style.borderBottomWidth = pageHeight + "px";
+    sobre.style.borderLeftWidth = pageWidth + "px";
+    
+    var tituloInscricao = document.getElementById("titulo-inscricao");
+    var tituloSobre = document.getElementById("titulo-o-evento");
+    
+    var menuHeight = document.getElementById("menu").offsetHeight;
+    tituloInscricao.style.top = "-" + (menuHeight/2 - 17) + "px";
+    tituloSobre.style.top = (menuHeight/2 - 70) + "px";
+    
+    var menuWidth = document.getElementById("menu").offsetWidth;
+    var pontoMedio = menuWidth/2;
+    
+    var wTituloInscricao = tituloInscricao.offsetWidth;
+    var leftTituloInscricao = pontoMedio - (wTituloInscricao - 20);
 
-    document.getElementById("titulo-inscricao").style.top = "-" + (pageHeight/2 - 17) + "px";
-    document.getElementById("titulo-inscricao").style.left = (pageWidth/4 + 26) + "px";
+    var leftTituloSobre = - pontoMedio - 20;
 
-    document.getElementById("titulo-o-evento").style.top = (pageHeight/2 + 30) + "px";
-    document.getElementById("titulo-o-evento").style.left = (pageWidth/2 - 140) + "px";
+    tituloInscricao.style.left = leftTituloInscricao + "px";
+    tituloSobre.style.left = leftTituloSobre + "px";
 });
