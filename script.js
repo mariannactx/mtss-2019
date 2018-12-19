ad47993aec295c3e6bbe8a9f96721c8904d1bec6
Marianna Cruz <mariannactx@gmail.com>
2018-12-19 14:54:41 -0300

    Animação de exibir/ocultar informaçao do menu inscricao-o-evento

--- script.js
+++ script.js
@@ -1,40 +1,29 @@
+var inscricao, sobre, tituloInscricao, tituloSobre;
 window.addEventListener("load", function(){
+    inscricao = document.getElementById("inscricao");
+    sobre = document.getElementById("o-evento");
+
+    tituloInscricao = document.getElementById("titulo-inscricao");
+    tituloSobre = document.getElementById("titulo-o-evento");
+
     sizes();
 });
 
 window.addEventListener("resize", sizes);
 
 function sizes(){
+    inscricao.style.transition = "";
+    sobre.style.transition = "";
+
     var pageWidth = window.innerWidth;
     var pageHeight = window.innerHeight - 66;
 
-    var inscricao = document.getElementById("inscricao");
-    var sobre = document.getElementById("o-evento");
-    
     inscricao.style.borderTopWidth = pageHeight + "px";
     inscricao.style.borderRightWidth = pageWidth + "px";
     
     sobre.style.borderBottomWidth = pageHeight + "px";
     sobre.style.borderLeftWidth = pageWidth + "px";
     
-    var tituloInscricao = document.getElementById("titulo-inscricao");
-    var tituloSobre = document.getElementById("titulo-o-evento");
-    
-    var menuHeight = document.getElementById("menu").offsetHeight;
-    tituloInscricao.style.top = "-" + (menuHeight/2 - 17) + "px";
-    tituloSobre.style.top = (menuHeight/2 - 70) + "px";
-    
-    var menuWidth = document.getElementById("menu").offsetWidth;
-    var pontoMedio = menuWidth/2;
-    
-    var wTituloInscricao = tituloInscricao.offsetWidth;
-    var leftTituloInscricao = pontoMedio - (wTituloInscricao - 20);
-
-    var leftTituloSobre = -pontoMedio;
-
-    tituloInscricao.style.left = leftTituloInscricao + "px";
-    tituloSobre.style.left = leftTituloSobre + "px";
-
     var b1, l1, b2, l2;
 
     b1 = pageHeight;
@@ -45,5 +34,39 @@ function sizes(){
 
     document.getElementById("complemento-titulo-o-evento").style.borderBottomWidth = b2 + "px";
     document.getElementById("complemento-titulo-o-evento").style.borderLeftWidth = l2 + "px";
-    document.getElementById("complemento-titulo-o-evento").style.left = (-l2 - 6) + "px";
+    document.getElementById("complemento-titulo-o-evento").style.left = (-l2 - 6) + "px"; 
+         
+    document.getElementById("menu").style.height = "";
+    var menuHeight = document.getElementById("menu").offsetHeight;
+    document.getElementById("menu").style.height = menuHeight + "px";
+    
+    tituloInscricao.style.top = "-" + (menuHeight/2 + 55) + "px";
+    tituloSobre.style.top = (menuHeight/2 - 55) + "px";
+    
+    var menuWidth = document.getElementById("menu").offsetWidth;
+    var pontoMedio = menuWidth/2;
+    
+    var wTituloInscricao = tituloInscricao.offsetWidth;
+    
+    var leftTituloInscricao = pontoMedio - (wTituloInscricao - l2);
+    var leftTituloSobre = - pontoMedio + ( (l2/2) + 10 );
+ 
+    tituloInscricao.style.left = leftTituloInscricao + "px";
+    tituloSobre.style.left = leftTituloSobre + "px";
+
+    inscricao.style.transition = ".5s all";
+    sobre.style.transition = ".5s all";
+}
+
+function showSection(section){
+    inscricao.style.borderTopWidth = 0; 
+    sobre.style.borderLeftWidth = "8500px";
+}
+
+function hideSection(){
+    var pageWidth = window.innerWidth;
+    var pageHeight = window.innerHeight - 66;
+
+    inscricao.style.borderTopWidth = pageHeight + "px";
+    sobre.style.borderLeftWidth = pageWidth + "px";
 }
