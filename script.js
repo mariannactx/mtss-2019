ae0a3fb3f15b065e3a6754ff29043f32eaaadaef
Marianna Cruz <mariannactx@gmail.com>
2018-12-21 09:52:05 -0300

    Ajustar posição e tamanhos dos titulos esquerda e direita para evitar diferenças entre telas

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-var inscricao, sobre, tituloInscricao, tituloSobre;
+var secaoEsquerda, secaoDireita, tituloEsquerda, tituloDireita;
 var pageWidth, pageHeight, headerHeight;
 
 window.addEventListener("load", function(){
-    inscricao = document.getElementById("inscricao");
-    sobre = document.getElementById("o-evento");
+    secaoEsquerda = document.getElementById("secao-esquerda");
+    secaoDireita = document.getElementById("secao-direita");
 
-    tituloInscricao = document.getElementById("titulo-inscricao");
-    tituloSobre = document.getElementById("titulo-o-evento");
+    tituloEsquerda = document.getElementById("titulo-esquerda");
+    tituloDireita = document.getElementById("titulo-direita");
 
     headerHeight = 53;
     sizes();
@@ -15,49 +15,47 @@ window.addEventListener("load", function(){
 window.addEventListener("resize", sizes);
 
 function sizes(){
-    inscricao.style.transition = "";
-    sobre.style.transition = "";
+    secaoEsquerda.style.transition = "";
+    secaoDireita.style.transition = "";
 
     pageWidth = window.innerWidth + 1;
     pageHeight = window.innerHeight - headerHeight;
 
-    inscricao.style.borderTopWidth = pageHeight + "px";
-    inscricao.style.borderRightWidth = pageWidth + "px";
+    secaoEsquerda.style.borderTopWidth = pageHeight + "px";
+    secaoEsquerda.style.borderRightWidth = pageWidth + "px";
     
-    sobre.style.borderBottomWidth = pageHeight + "px";
-    sobre.style.borderLeftWidth = pageWidth + "px";
+    secaoDireita.style.borderBottomWidth = pageHeight + "px";
+    secaoDireita.style.borderLeftWidth = pageWidth + "px";
+     
+    var menu = document.getElementById("menu");
     
+    menu.style.height = "";
+    var menuHeight = menu.offsetHeight;
+    menu.style.height = menuHeight + "px";
+    
+    var menuWidth = menu.offsetWidth;
+    var pontoMedio = menuWidth/2;
+    
+    tituloEsquerda.style.top = "-" + (menuHeight/2 - 1) + "px";
+    tituloEsquerda.style.left = (pontoMedio - tituloEsquerda.offsetWidth)  + "px";
+ 
+    tituloDireita.style.top = (menuHeight/2) + "px";
+    tituloDireita.style.left = - (pontoMedio + 3) + "px";
+
     var b1, l1, b2, l2;
 
     b1 = pageHeight;
     l1 = pageWidth;
-    b2 = tituloSobre.offsetHeight;
+    b2 = tituloDireita.offsetHeight;
     l2 = (b2 * l1)/b1;
+    
+    document.getElementById("complemento").style.borderBottomWidth = (b2 - 0.2) + "px";
+    document.getElementById("complemento").style.borderLeftWidth = l2 + "px";
+    document.getElementById("complemento").style.left = -l2 + "px"; 
 
-    document.getElementById("complemento-titulo-o-evento").style.borderBottomWidth = (b2)  + "px";
-    document.getElementById("complemento-titulo-o-evento").style.borderLeftWidth = (l2 - 5) + "px";
-    document.getElementById("complemento-titulo-o-evento").style.left = (-l2 + 5) + "px"; 
-         
-    document.getElementById("menu").style.height = "";
-    var menuHeight = document.getElementById("menu").offsetHeight;
-    document.getElementById("menu").style.height = menuHeight + "px";
-    
-    var menuWidth = document.getElementById("menu").offsetWidth;
-    var pontoMedio = menuWidth/2;
-    
-    var wTituloInscricao = tituloInscricao.offsetWidth;
-    
-    var leftTituloInscricao = pontoMedio - (wTituloInscricao) - 20;
-    var leftTituloSobre = - pontoMedio - (pageWidth < 500 ? 40 : 30);
- 
-    tituloInscricao.style.top = "-" + (menuHeight/2 - 1) + "px";
-    tituloInscricao.style.left = (leftTituloInscricao)  + "px";
- 
-    tituloSobre.style.top = (menuHeight/2) + "px";
-    tituloSobre.style.left = leftTituloSobre + "px";
 
-    inscricao.style.transition = ".6s all";
-    sobre.style.transition = ".5s all";
+    secaoEsquerda.style.transition = ".6s all";
+    secaoDireita.style.transition = ".5s all";
 }
 
 function showSection(show, hide){
@@ -65,20 +63,19 @@ function showSection(show, hide){
     document.getElementById(show+"-content").style.display = "block";
     document.getElementById(hide+"-content").style.display = "none";
 
-    inscricao.style.borderTopWidth = "40px"; 
-    sobre.style.borderLeftWidth = "8500px";
+    secaoEsquerda.style.borderTopWidth = "40px"; 
+    secaoDireita.style.borderLeftWidth = "8500px";
 
-    tituloInscricao.style.opacity = 0;
-    tituloSobre.style.opacity = 0;
+    tituloEsquerda.style.opacity = 0;
+    tituloDireita.style.opacity = 0;
 
     setTimeout(hideSection, 2000);
 }
 
 function hideSection(){
-    inscricao.style.borderTopWidth = pageHeight + "px";
-    sobre.style.borderLeftWidth = pageWidth + "px";
+    secaoEsquerda.style.borderTopWidth = pageHeight + "px";
+    secaoDireita.style.borderLeftWidth = pageWidth + "px";
 
-
-    tituloInscricao.style.opacity = 1;
-    tituloSobre.style.opacity = 1;
+    tituloEsquerda.style.opacity = 1;
+    tituloDireita.style.opacity = 1;
 }
