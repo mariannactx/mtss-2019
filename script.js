075f6fb3c7e5d7fa56bb4ee22323d82f5cd06e41
Marianna Cruz <mariannactx@gmail.com>
2018-12-28 10:56:39 -0300

    Adicionar e ajustar conteúdo das seções. Ajustar barra superior e inferior. Ajustar animação do menu.

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-var secaoEsquerda, secaoDireita, tituloEsquerda, tituloDireita;
-var pageWidth, pageHeight, headerHeight;
+var menuEsquerda, menuDireita, tituloEsquerda, tituloDireita;
+var pageWidth, pageHeight, headerHeight, b1, l1, b2, l2, b3, l3;
 
 window.addEventListener("load", function(){
-    secaoEsquerda = document.getElementById("secao-esquerda");
-    secaoDireita = document.getElementById("secao-direita");
-
-    tituloEsquerda = document.getElementById("titulo-esquerda");
-    tituloDireita = document.getElementById("titulo-direita");
+    
+    menuEsquerda    = document.getElementById("menu-esquerda");
+    menuDireita     = document.getElementById("menu-direita");
+    tituloEsquerda  = document.getElementById("titulo-esquerda");
+    tituloDireita   = document.getElementById("titulo-direita");
+    contentEsquerda = document.getElementById("content-esquerda");    
+    contentDireita  = document.getElementById("content-direita");
 
     headerHeight = 53;
     sizes();
@@ -15,30 +17,33 @@ window.addEventListener("load", function(){
 window.addEventListener("resize", sizes);
 
 function sizes(){
-    secaoEsquerda.style.transition = "";
-    secaoDireita.style.transition = "";
+    menuEsquerda.style.transition = "";
+    menuDireita.style.transition = "";
 
     pageWidth = window.innerWidth + 1;
-    pageHeight = window.innerHeight - headerHeight;
+    pageHeight = window.innerHeight - 1 - headerHeight;
 
-    secaoEsquerda.style.borderTopWidth = pageHeight + "px";
-    secaoEsquerda.style.borderRightWidth = pageWidth + "px";
+    menuEsquerda.style.borderTopWidth = pageHeight + "px";
+    menuEsquerda.style.borderRightWidth = pageWidth + "px";
     
-    secaoDireita.style.borderBottomWidth = pageHeight + "px";
-    secaoDireita.style.borderLeftWidth = pageWidth + "px";
+    menuDireita.style.borderBottomWidth = pageHeight + "px";
+    menuDireita.style.borderLeftWidth = pageWidth + "px";
      
     var menu = document.getElementById("menu");
+    var content = document.getElementById("content");
     
     menu.style.height = "";
+    content.style.height = "";
     var menuHeight = menu.offsetHeight;
     menu.style.height = menuHeight + "px";
-        
-    var b1 = pageHeight;
-    var l1 = pageWidth;
-    var b2 = tituloEsquerda.children[0].offsetHeight / 1.5;
-    var b3 = tituloDireita.children[1].offsetHeight;
-    var l2 = (b2 * l1)/b1;
-    var l3 = (b3 * l1)/b1;
+    content.style.height = menuHeight + "px";
+
+    b1 = pageHeight;
+    l1 = pageWidth;
+    b2 = tituloEsquerda.children[0].offsetHeight / 1.5;
+    b3 = tituloDireita.children[1].offsetHeight;
+    l2 = (b2 * l1)/b1;
+    l3 = (b3 * l1)/b1;
 
     var pontoMedioX = pageWidth/2;
     var pontoMedioY = pageHeight/2;
@@ -59,28 +64,36 @@ function sizes(){
     tituloDireita.children[0].style.borderBottomWidth = b3 + "px";
     tituloDireita.children[0].style.borderLeftWidth = l3 + "px";
 
-    secaoEsquerda.style.transition = ".6s all";
-    secaoDireita.style.transition = ".5s all";
+    menuEsquerda.style.transition = ".6s all";
+    menuDireita.style.transition = ".5s all";
 }
 
 function showSection(show, hide){
-
+    
+    menu.style.background = "transparent"; 
+    
     document.getElementById("content-" + show).style.display = "block";
     document.getElementById("content-" + hide).style.display = "none";
-
-    secaoEsquerda.style.borderTopWidth = "40px"; 
-    secaoDireita.style.borderLeftWidth = "8500px";
-
+    
+    menuEsquerda.style.borderTopWidth = "0px"; 
+    menuDireita.style.borderLeftWidth = pageWidth*5 + "px";
+    
+    menuEsquerda.style.opacity  = 0;
+    menuDireita.style.opacity   = 0
     tituloEsquerda.style.opacity = 0;
-    tituloDireita.style.opacity = 0;
-
-    setTimeout(hideSection, 2000);
+    tituloDireita.style.opacity  = 0;
+    
+    setTimeout(function(){ menu.style.visibility = "hidden" }, 600);
 }
 
 function hideSection(){
-    secaoEsquerda.style.borderTopWidth = pageHeight + "px";
-    secaoDireita.style.borderLeftWidth = pageWidth + "px";
+    menu.style.visibility = "visible";
+    menu.style.background = "#fff"; 
+    menuEsquerda.style.borderTopWidth = pageHeight + "px";
+    menuDireita.style.borderLeftWidth = pageWidth + "px";
 
+    menuEsquerda.style.opacity  = 1;
+    menuDireita.style.opacity   = 1;
     tituloEsquerda.style.opacity = 1;
-    tituloDireita.style.opacity = 1;
+    tituloDireita.style.opacity  = 1;
 }
