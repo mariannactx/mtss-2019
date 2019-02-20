21815ac12f26283c6dc0c190f11c701b202df8fb
Marianna Cruz <mariannactx@gmail.com>
2019-02-19 21:48:32 -0300

    Manter estilos para a newsletter

--- apoio.php
+++
@@ -1,8 +0,0 @@
-<h3 class="content-titulo">
-    <span>APOIO</span>
-</h3>
-<div class="content-texto">
-
-   Apoio Conductor      
-    
-</div>
--- como-chegar.php
+++
@@ -1,6 +0,0 @@
-<h3 class="content-titulo">
-    <span>COMO CHEGAR</span>
-</h3>
-<div class="content-texto">
-    <iframe id="googlemaps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.118530067141!2d-34.84560455018402!3d-7.112260294838725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7acdd46de96eecf%3A0x4151e5a8e7df7079!2sConductor+Tecnologia!5e0!3m2!1spt-BR!2sbr!4v1546267900796" frameborder="0" style="border:0" allowfullscreen></iframe>
-</div>
