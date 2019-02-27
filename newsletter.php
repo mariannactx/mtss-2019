3e3775a1a7f4791c4efce58c6a703c6441bf2a7e
Marianna Cruz <mariannactx@gmail.com>
2019-02-26 22:26:22 -0300

    Formulário de inscrição na newsletter

--- newsletter.php
+++ newsletter.php
@@ -1,6 +1,7 @@
-<form action="https://mulhertechsim.us20.list-manage.com/subscribe/post?u=d7e39d6142bb2cd59db7ea548&amp;id=892c95f635">
-                    <input type="text" name="email" placeholder="email" />
-                    <button type="submit" class="text-dark-green"> QUERO NOVIDADES 
-                        SEM SPAM! </button>
-                </form> 
-                
+<form action="https://mulhertechsim.us20.list-manage.com/subscribe/post?u=d7e39d6142bb2cd59db7ea548&amp;id=892c95f635" method="post" target="_blank" novalidate>
+    <input type="text" name="EMAIL" value="" placeholder="email" />
+    <div style="position: absolute; left: -5000px;" aria-hidden="true">
+        <input type="text" name="b_d7e39d6142bb2cd59db7ea548_892c95f635" tabindex="-1" value="">
+    </div>
+    <button type="submit" class="text-dark-green"> QUERO NOVIDADES SEM SPAM! </button>
+</form>
