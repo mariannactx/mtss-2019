c6fe06143d2d34ed50dedb7a96f99a17e95aacce
Marianna Cruz <mariannactx@gmail.com>
2019-03-15 12:13:21 -0300

    Ajustar programação e adicionar descrição completa de Dandara.

--- programacao.php
+++ programacao.php
@@ -15,7 +15,25 @@
             <div class="timeline__content">
                 <h2 class="no-indent"><?=$atividade['titulo']?></h2>
                 <?php if(!empty($atividade['palestrante'])): ?>
-                    <h3 class="no-indent"><?=$atividade['palestrante']?></h3>
+                    <?php
+                        $contato  = $atividade['contato'];
+                        $twitter  = $contato['twitter'];
+                        $github   = $contato['github'];
+                        $linkedin = $contato['linkedin'];
+                        
+                    ?>
+                    <h3 class="no-indent">
+                        <?=$atividade['palestrante']?>
+                        <?php if($twitter) : ?>
+                            <a href="http://www.twitter.com/<?=$twitter?>"<i class="fab fa-twitter"></i></a>
+                        <?php endif ?>
+                        <?php if($github) : ?>
+                            <a href="http://www.github.com/<?=$github?>"<i class="fab fa-github"></i></a>
+                        <?php endif ?>
+                        <?php if($linkedin) : ?>
+                            <a href="http://www.linkedin.com/in/<?=$linkedin?>"<i class="fab fa-linkedin"></i></a>
+                        <?php endif ?>
+                    </h3>
                 <?php endif;?>
                 <span class="timeline__date no-indent"><?=$atividade['hora']?></span>
                 <?php if(!empty($atividade['descricao'])): ?>
